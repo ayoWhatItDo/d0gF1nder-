923be11d9cd954cff9c7f264b1281b20ef93ec43
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-08-31 03:45:41 -0700

    Update FilterDogDetails.js

--- FilterDogDetails.js
+++ FilterDogDetails.js
@@ -2,20 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import DogDetails from './DogDetails';
 
-
-/**
- * FilterDogDetails finds a dog by name or renders null
- *
- * state: none
- *
- * props:
-  * dogs: [{name, src}]
- *
- *
-  * Rendered at /dogs/:name
- *
- */
-
 function FilterDogDetails({ dogs }) {
   const { name } = useParams();
 
